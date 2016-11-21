2abb179ace059599ab03b5a433a51f9648dc4c24
Pedro Morgado <pedrombmorgado@gmail.com>
2016-11-21 14:40:02 +0000

    fix version of poise python

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.5'
 
 
-depends 'poise-python', '~> 1.4.6'
+depends 'poise-python', '~> 1.5.1'
 supports 'centos', '~> 6.0'
 supports 'amazon'
