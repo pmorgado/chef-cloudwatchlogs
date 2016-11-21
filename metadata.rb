7663e7325f907024f514525f94f87fa841787b6a
Pedro Morgado <pedrombmorgado@gmail.com>
2016-11-21 12:18:18 +0000

    changed platform supported

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
-name 'aws-cloudwatchlogs'
-maintainer 'Alexandre Guimarães Malucelli'
-maintainer_email 'alexandre@malucelli.net'
+name             'aws-cloudwatchlogs'
+maintainer       'Pedro Morgado'
 license 'Apache 2.0'
-description 'Installs/Configures AWS CloudWatch Logs Agent'
+maintainer_email 'pedrombmorgado@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures AWS CloudWatch Logs Agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.4'
-source_url 'https://github.com/amalucelli/chef-cloudwatchlogs' if respond_to? :source_url
-issues_url 'https://github.com/amalucelli/chef-cloudwatchlogs/issues' if respond_to? :issues_url
+version          '1.1.5'
+
+
+supports 'centos', '~> 6.0'
+supports 'amazon'
